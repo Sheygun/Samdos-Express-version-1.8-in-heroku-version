cc4a3e9fdf023662a180779f72a1f3071b226aa6
Olabayo Segun Emmanuel <hollasheg@gmail.com>
2019-04-30 12:36:37 -0400

    push to github

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
+
+
   .get('/', (req, res) => res.render('index'))
 
   // ======================= index section ================================
